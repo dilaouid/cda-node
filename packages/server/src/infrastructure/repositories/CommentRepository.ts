6259e0abdaf7ee807e2c67d8af01adfee80bd391
dilaouid <dilaouid@student.42.fr>
2024-05-13 17:02:05 +0200

    update support

--- packages/server/src/infrastructure/repositories/CommentRepository.ts
+++ packages/server/src/infrastructure/repositories/CommentRepository.ts
@@ -1,29 +1,86 @@
-import { Comment } from "../../domain/entities/Comment";
-import fs from 'fs';
-import path from "path";
+import { db } from "../data";
+import { Comment, NewComment } from "../../domain/entities/Comment";
+import { comments, posts, users } from "../data/schema";
+import { and, eq } from "drizzle-orm";
 
 /**
  * Repository qui gère le CRUD des commentaires
  */
 export class CommentRepository {
-    private filePath = path.join(__dirname, '..', 'data', 'comments.json');
+
+    getCommentById(id: string) {
+        try {
+            return db.select({
+                id: comments.id,
+                content: comments.content,
+                date: comments.date,
+                author: {
+                    id: users.id,
+                    username: users.username
+                }
+            }).from(comments)
+            .leftJoin(
+                users, eq(users.id, comments.author)
+            ).where(
+                eq(comments.id, id)
+            ).execute();
+        } catch(err) {
+            console.error(err);
+            throw new Error('Impossible de récupérer le commentaire');
+        }
+    }
+
+    deleteCommentById(id: string, userId: string) {
+        try {
+            return db.delete(comments).where(
+                and(
+                    eq(comments.id, id),
+                    eq(comments.author, userId)
+                )
+            ).execute();
+        } catch(err) {
+            console.error(err);
+            throw new Error('Impossible de supprimer le commentaire');
+        }
+    }
+
+    createComment(comment: NewComment) {
+        try {
+            return db.insert(comments).values(comment).execute();
+        } catch (err) {
+            console.error(err);
+            throw new Error('Impossible de créer le commentaire');
+        }
+    }
+
 
     /**
      * Récupère tous les commentaires du fichier comments.json
-     * @returns {Comment[]}
      */
-    getAllComments(): Comment[] {
-        const data = fs.readFileSync(this.filePath, 'utf-8');
-        return JSON.parse(data);
+    getAllComments() {
+        try {
+            return db.select({
+                id: comments.id,
+                content: comments.content,
+                date: comments.date,
+                post: {
+                    id: posts.id,
+                    title: posts.title
+                },
+                author: {
+                    id: users.id,
+                    username: users.username
+                }
+            }).from(comments)
+            .leftJoin(
+                users, eq(users.id, comments.author)
+            ).leftJoin(
+                posts, eq(posts.id, comments.postId)
+            ).execute();
+        } catch(err) {
+            console.error(err);
+            throw new Error('Impossible de récupérer les commentaires');
+        }
     }
 
-    /**
-     * // Récupère un commentaire par son id
-     * @param {string} postId - identifiant unique du commentaire
-     * @returns {Comment[]}
-     */
-    getCommentsByPostId(postId: string): Comment[] {
-        const comments = this.getAllComments();
-        return comments.filter(comment => comment.postId === postId);
-    }
 }
