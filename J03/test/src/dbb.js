9f108d165d3df6f3c6b6a6099b05bf124d860463
dilaouid <dilaouid@student.42.fr>
2024-05-13 10:35:42 +0200

    archi

--- J03/test/src/dbb.js
+++
@@ -1,9 +0,0 @@
-function getData(){
-    console.log("utilisation de la fonction getData"); 
-    return [
-        {id : 1 , titre :"Article 1"},
-        {id : 2 , titre :"Article 2"}
-    ]
-}
-
-export default getData 
